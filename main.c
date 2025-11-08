8930e0b612efc54466729da7c54a8832ccfba892
r0p <r0p@localhost.localdomain>
2025-11-08 16:04:35 +0100

    added bias parameter

--- main.c
+++ main.c
@@ -19,11 +19,11 @@ float rand_float(void)
     return (float) rand()/ (float) RAND_MAX;
 }
 
-float cost(float w) {
+float cost(float w, float b) {
     float result = 0.0f;
     for (size_t i=0; i<train_count; i++) {
         float x = train[i][0];
-        float y = x*w;
+        float y = x*w + b;
         float d = y - train[i][1];
         result += d*d;
     }
@@ -37,13 +37,17 @@ int main()
     srand(69);
     // y = x*w;
     float w = rand_float() * 10.0f; // by default 10.0 is treated as a double, 10.0f ensures that 10.0 is treated as float.
+    float b = rand_float() * 5.0f;
     float eps = 1e-3;
     float rate = 1e-3;
-    printf("result = %f\n", cost(w));
-    for (size_t i=0; i<500; i++) {
-        float dcost = (cost(w+eps) - cost(w))/eps;
-        w -= rate*dcost;
-        printf("result = %f\n", cost(w));
+    printf("result = %f\n", cost(w, b));
+    for (size_t i=0; i<5000; i++) {
+        float c = cost(w, b);
+        float dw = (cost(w+eps, b) - c)/eps;
+        float db = (cost(w, b+eps) - c)/eps;
+        w -= rate*dw;
+        b -= rate*db;
+        printf("result = %f, w = %f, b = %f\n", cost(w, b), w, b);
     }
     printf("------------------------------\n");
     printf("w = %f\n", w);
